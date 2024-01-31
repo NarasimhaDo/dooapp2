d8b0616bda126f4d0dc0f90ebdf8df9977cb4776
Narasimha <narasimhududuvvvuru@gmail.com>
2024-01-31 19:07:18 +0530

    install pods

--- dooapp2/dooapp2/RStrings.swift
+++ dooapp2/dooapp2/RStrings.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 import Foundation
+import SocketIO
+
 @objc public class RandomStrings : NSObject {
     public override init(){}
     @objc public class func getrandomstring() -> String
     {
         // here get dfd
         return UUID().uuidString
+    }
+    
+    func socketConnected(is_add : String) -> Bool
+    {
+        if is_add == "add" {
+           return true
+        }
+        else{
+            return false
+        }
     }
 }
